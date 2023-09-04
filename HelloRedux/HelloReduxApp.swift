01e5918f8a55beb9aa03d33630103d61999d664e
ptejas26 <ptejas.patelia@gmail.com>
2023-09-03 23:42:15 -0400

    Initial Commit

---
+++ HelloRedux/HelloReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloReduxApp.swift
+//  HelloRedux
+//
+//  Created by Tejas on 2023-09-03.
+//
+
+import SwiftUI
+
+@main
+struct HelloReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
